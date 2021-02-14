190b29bd9252a77daf963b1337f18f53b8259078
Daylight <kousuke.tokita@gmail.com>
2021-02-14 12:06:38 +0900

    :sparkles: 有効化、無効化時のメッセージを追加。

--- src/main/java/jp/pro/daylight/minecraftplugin/MinecraftPlugin.java
+++ src/main/java/jp/pro/daylight/minecraftplugin/MinecraftPlugin.java
@@ -7,11 +7,12 @@ public final class MinecraftPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        getLogger().info("プラグインが有効化されました。");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("プラグラインが無効化されました。");
     }
 }
